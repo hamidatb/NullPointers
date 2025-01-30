44de7bc9e8eb1986875194b7987821773e6675c9
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-01-29 17:50:33 -0700

    Create abstract Shape class

---
+++ app/src/main/java/com/hamidat/nullpointers/Shape.java
@@ -0,0 +1,22 @@
+package com.hamidat.nullpointers;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    // Constructor for the abstract class
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
